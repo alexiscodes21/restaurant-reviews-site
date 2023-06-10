2d2dc0087b0f4eacb6b66731d1dbd64ce66a1952
Alexis Winters <alexiscodes@yahoo.com>
2023-06-10 14:24:57 -0500

    the app component and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import PlantopiaLogo from "./assets/img/plantopia.png";
+
 
 function App() {
   return (
     <div className="App">
-        Restaurant Reviews Site
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={PlantopiaLogo} alt="Plantopia Logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      Plantopia
     </div>
   );
 }
