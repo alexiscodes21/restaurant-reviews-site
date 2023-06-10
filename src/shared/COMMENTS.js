10c14d4b8e513b92af953c34b8a221d7207893d8
Alexis Winters <alexiscodes@yahoo.com>
2023-06-10 10:41:25 -0500

    update restaurants array

--- src/shared/COMMENTS.js
+++ src/shared/COMMENTS.js
@@ -1,162 +1,162 @@
-export const COMMENTS = [
-    {
-        id: 0,
-        campsiteId: 0,
-        rating: 5,
-        text: 'The grass was indeed greener here than our last campsite.',
-        author: 'Simon Hunt',
-        date: '2018-10-25T16:30Z'
-    },
-    {
-        id: 1,
-        campsiteId: 0,
-        rating: 3,
-        text: 'This lone monkey kept following us back to our tent.',
-        author: 'Zandra Katherine',
-        date: '2017-06-17T03:33Z'
-    },
-    {
-        id: 2,
-        campsiteId: 0,
-        rating: 4,
-        text: 'Do not feed the wildlife. They will follow you back to the tent.',
-        author: 'Amy Keophilavong',
-        date: '2019-02-18T18:12Z'
-    },
-    {
-        id: 3,
-        campsiteId: 0,
-        rating: 3,
-        text: "Avoid Sundays; couldn't relax and play my flute with all the noisy kids.",
-        author: 'Jean Luc',
-        date: '2019-08-04T20:11Z'
-    },
-    {
-        id: 4,
-        campsiteId: 0,
-        rating: 5,
-        text: "This has been our favorite campsite for over 7 years. It is an absolutely beautiful site. The hosts are very friendly and helpful. Don't forget to try out the lake.",
-        author: 'R. E. Osariemen',
-        date: '2018-07-23T19:44Z'
-    },
-    {
-        id: 5,
-        campsiteId: 1,
-        rating: 4,
-        text: "There's nothing more fun than setting up a tent in a rainstorm.",
-        author: 'Juan Cortez',
-        date: '2017-06-17T03:33Z'
-    },
-    {
-        id: 6,
-        campsiteId: 1,
-        rating: 5,
-        text: 'I caught a world-record-breaking trout here on my last visit.',
-        author: 'McKenzie Sebastian',
-        date: '2019-02-18T18:12Z'
-    },
-    {
-        id: 7,
-        campsiteId: 1,
-        rating: 5,
-        text: 'The campground was beautiful, but the bugs could bite sometimes.',
-        author: 'Brennen Ech',
-        date: '2019-08-04T20:11Z'
-    },
-    {
-        id: 8,
-        campsiteId: 1,
-        rating: 4,
-        text: "Love the river but I didn't catch any fish.",
-        author: 'Amy Keophilavong',
-        date: '2018-07-23T19:44Z'
-    },
-    {
-        id: 9,
-        campsiteId: 1,
-        rating: 5,
-        text: 'A wonderful place to reconnect with nature.',
-        author: 'Jordan Runn',
-        date: '2019-08-04T20:11Z'
-    },
-    {
-        id: 10,
-        campsiteId: 2,
-        rating: 5,
-        text: 'The Frozen Tundra is great place for family and friends camping!',
-        author: 'Olaf the Snowman',
-        date: '2019-02-18T18:12Z'
-    },
-    {
-        id: 11,
-        campsiteId: 2,
-        rating: 1,
-        text: 'We had to leave about 2am to go back to the city because the quiet was just too much!',
-        author: 'Vincent and Mona Lisa Gambini',
-        date: '2018-10-25T16:30Z'
-    },
-    {
-        id: 12,
-        campsiteId: 2,
-        rating: 5,
-        text: 'The stars at night were a revelation!',
-        author: 'Ann Dabramov',
-        date: '2018-07-23T19:44Z'
-    },
-    {
-        id: 13,
-        campsiteId: 2,
-        rating: 4,
-        text: 'Great scenic hiking trails but it was challenging to go up the mountain.',
-        author: 'Amy Keophilavong',
-        date: '2018-10-25T16:30Z'
-    },
-    {
-        id: 14,
-        campsiteId: 2,
-        rating: 5,
-        text: 'Perfect scenery for a smoked kipper breakfast with friends!',
-        author: 'Dave Lister',
-        date: '2019-08-04T20:11Z'
-    },
-    {
-        id: 15,
-        campsiteId: 3,
-        rating: 4,
-        text: 'Just remember a bear can run and climb faster than you...',
-        author: 'Markell Richards',
-        date: '2017-06-17T03:33Z'
-    },
-    {
-        id: 16,
-        campsiteId: 3,
-        rating: 5,
-        text: 'Excellent site to recharge from the city.',
-        author: 'Amy Keophilavong',
-        date: '2018-07-23T19:44Z'
-    },
-    {
-        id: 17,
-        campsiteId: 3,
-        rating: 5,
-        text: 'What a magnificent view!',
-        author: 'Tinus Lorvaldes',
-        date: '2018-10-25T16:30Z'
-    },
-    {
-        id: 18,
-        campsiteId: 3,
-        rating: 4,
-        text: 'The sites are huge! We felt like we were in the middle of nowhere, in a good way.',
-        author: 'Chloe Haskins',
-        date: '2017-06-17T03:33Z'
-    },
-    {
-        id: 19,
-        campsiteId: 3,
-        rating: 5,
-        text: "The photos don't do justice to the beauty of this place.",
-        author: 'Minae Lee',
-        date: '2019-02-18T18:12Z'
-    }
+export const RESTAURANTS = [
+  {
+    id: 0,
+    restaurantId: 0,
+    rating: 5,
+    text: "I had the most incredible vegan burger at Leafy Bites! The patty was perfectly seasoned, and the toppings were so flavorful. Definitely coming back for more!",
+    author: "EmilyGreenEats",
+    date: "2018-10-25T16:30Z",
+  },
+  {
+    id: 1,
+    restaurantId: 0,
+    rating: 5,
+    text: "Leafy Bites has the best vegan desserts in town! Their chocolate cake is moist, rich, and absolutely heavenly. A must-visit for any dessert lover!",
+    author: "SweetToothVegan",
+    date: "2017-06-17T03:33Z",
+  },
+  {
+    id: 2,
+    restaurantId: 0,
+    rating: 5,
+    text: "As someone new to vegan food, Leafy Bites exceeded my expectations! Their friendly staff helped me choose delicious options, and I was amazed by the flavors. Highly recommend!",
+    author: "VeggieExplorer",
+    date: "2019-02-18T18:12Z",
+  },
+  {
+    id: 3,
+    restaurantId: 0,
+    rating: 5,
+    text: "Leafy Bites offers a fantastic brunch menu! The tofu scramble and avocado toast are delightful. Such a cozy atmosphere too. Perfect for lazy Sunday mornings!",
+    author: "BrunchLover123",
+    date: "2019-08-04T20:11Z",
+  },
+  {
+    id: 4,
+    restaurantId: 0,
+    rating: 5,
+    text: "I can't get enough of Leafy Bites' fresh salads! The ingredients are so vibrant, and the dressings are bursting with flavor. Eating healthy never tasted this good!",
+    author: "SaladSensation",
+    date: "2018-07-23T19:44Z",
+  },
+  {
+    id: 5,
+    restaurantId: 1,
+    rating: 4,
+    text: "The creativity at Herb Haven is unparalleled! Their dishes are true works of art. I was blown away by their innovative use of herbs and spices. A culinary adventure!",
+    author: "FlavorExplorer",
+    date: "2017-06-17T03:33Z",
+  },
+  {
+    id: 6,
+    restaurantId: 1,
+    rating: 5,
+    text: "Herb Haven is a vegan food paradise. The flavors are so well-balanced, and you can taste the love and passion in every bite. Truly a gem in the vegan scene!",
+    author: "FoodieDreamer",
+    date: "2019-02-18T18:12Z",
+  },
+  {
+    id: 7,
+    restaurantId: 1,
+    rating: 5,
+    text: "I've never tasted such incredible herb-infused drinks before! Herb Haven's mocktails are refreshing, aromatic, and perfect for a night out with friends. Cheers to that!",
+    author: "MocktailMaven",
+    date: "2019-08-04T20:11Z",
+  },
+  {
+    id: 8,
+    restaurantId: 1,
+    rating: 4,
+    text: "Herb Haven is my go-to place for a special occasion. The fine dining experience is top-notch, and their plant-based creations rival any gourmet restaurant. Exceptional!",
+    author: "FineDiningEnthusiast",
+    date: "2018-07-23T19:44Z",
+  },
+  {
+    id: 9,
+    restaurantId: 1,
+    rating: 5,
+    text: "Herb Haven's desserts are simply divine! Their lavender-infused cheesecake is a dream come true. I'm always excited to try their seasonal dessert offerings.",
+    author: "DessertLover22",
+    date: "2019-08-04T20:11Z",
+  },
+  {
+    id: 10,
+    restaurantId: 2,
+    rating: 5,
+    text: "Root & Stem serves the most flavorful vegan comfort food. The mushroom bourguignon and truffle mac 'n' cheese are my absolute favorites. So satisfying!",
+    author: "ComfortFoodCraver",
+    date: "2019-02-18T18:12Z",
+  },
+  {
+    id: 11,
+    restaurantId: 2,
+    rating: 1,
+    text: "Root & Stem's attention to detail is remarkable. Every dish is beautifully presented and bursting with complex flavors. An extraordinary culinary experience!",
+    author: "EpicureanExplorer",
+    date: "2018-10-25T16:30Z",
+  },
+  {
+    id: 12,
+    restaurantId: 2,
+    rating: 5,
+    text: "I can't get over the creativity at Root & Stem! Their jackfruit tacos are a game-changer. The texture and seasoning are spot on. A must-try for any taco lover!",
+    author: "TacoFanatic",
+    date: "2018-07-23T19:44Z",
+  },
+  {
+    id: 13,
+    restaurantId: 2,
+    rating: 4,
+    text: "Root & Stem is a haven for foodies seeking plant-based delights. Their seasonal menu showcases the best of what nature has to offer. An absolute treat!",
+    author: "FoodieAdventures",
+    date: "2018-10-25T16:30Z",
+  },
+  {
+    id: 14,
+    restaurantId: 2,
+    rating: 5,
+    text: "Root & Stem is my favorite vegan date night spot. The romantic ambiance, impeccable service, and delectable dishes make it a perfect place for a special evening.",
+    author: "RomanticDiner",
+    date: "2019-08-04T20:11Z",
+  },
+  {
+    id: 15,
+    restaurantId: 3,
+    rating: 4,
+    text: "Garden Delights is a slice of paradise for vegans! Their quinoa-stuffed bell peppers and grilled vegetable skewers are bursting with freshness and flavor. So satisfying!",
+    author: "VeggieCraver",
+    date: "2017-06-17T03:33Z",
+  },
+  {
+    id: 16,
+    restaurantId: 3,
+    rating: 5,
+    text: "Garden Delights' smoothie bowls are a great way to start the day. The bowls are packed with nutrient-rich ingredients, and the toppings are a delightful blend of textures.",
+    author: "SmoothieLover88",
+    date: "2018-07-23T19:44Z",
+  },
+  {
+    id: 17,
+    restaurantId: 3,
+    rating: 5,
+    text: "Garden Delights' weekend brunch is a must-try! Their tofu scramble, hash browns, and freshly squeezed juices are the perfect combination for a lazy Sunday morning.",
+    author: "BrunchHopper",
+    date: "2018-10-25T16:30Z",
+  },
+  {
+    id: 18,
+    restaurantId: 3,
+    rating: 4,
+    text: "Garden Delights' outdoor seating area is a hidden gem. Surrounded by lush greenery, enjoying a delicious vegan meal feels like a tranquil escape from the city.",
+    author: "NatureLover123",
+    date: "2017-06-17T03:33Z",
+  },
+  {
+    id: 19,
+    restaurantId: 3,
+    rating: 5,
+    text: "Garden Delights' salads are a symphony of flavors. The combination of crisp greens, vibrant veggies, and zesty dressings leaves my taste buds dancing with joy. Love it!",
+    author: "SaladEnthusiast2022",
+    date: "2019-02-18T18:12Z",
+  },
 ];
