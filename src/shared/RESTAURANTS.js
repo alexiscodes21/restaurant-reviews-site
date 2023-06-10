fa0d4f1339d13e48315e787d721d391d21774e08
Alexis Winters <alexiscodes@yahoo.com>
2023-06-10 11:30:42 -0500

    update restaurants array

--- src/shared/RESTAURANTS.js
+++ src/shared/RESTAURANTS.js
@@ -1,43 +1,42 @@
-import reactLakeImg from '../assets/img/react-lake.jpg';
-import chromeRiverImg from '../assets/img/chrome-river.jpg';
-import breadCrumbImg from '../assets/img/breadcrumb-trail.jpg';
-import reduxWoodsImg from '../assets/img/redux-woods.jpg';
+import leafyBites from "../assets/img/leafy-bites.jpg";
+import herbHaven from "../assets/img/herb-haven.jpg";
+import rootAndStem from "../assets/img/root-and-stem.jpg";
+import gardenDelights from "../assets/img/garden-delights.jpg";
 
 export const RESTAURANTS = [
-    {
-        id: 0,
-        name: 'React Lake Campground',
-        image: reactLakeImg,
-        elevation: 1233,
-        featured: false,
-        description:
-            'Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.'
-    },
-    {
-        id: 1,
-        name: 'Chrome River Campground ',
-        image: chromeRiverImg,
-        elevation: 877,
-        featured: false,
-        description:
-            'Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.'
-    },
-    {
-        id: 2,
-        name: 'Breadcrumb Trail Campground',
-        image: breadCrumbImg,
-        elevation: 2901,
-        featured: false,
-        description:
-            'Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.'
-    },
-    {
-        id: 3,
-        name: 'Redux Woods Campground',
-        image: reduxWoodsImg,
-        elevation: 42,
-        featured: true,
-        description:
-            "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
-    }
+  {
+    id: 0,
+    name: "Leafy Bites",
+    image: leafyBites,
+    elevation: 1233,
+    featured: false,
+    description: "A cozy and casual vegan restaurant offering a wide range of plant-based dishes made with fresh, locally sourced ingredients.",
+  },
+  {
+    id: 1,
+    name: "Herb Haven",
+    image: herbHaven,
+    elevation: 877,
+    featured: false,
+    description:
+      "A modern and vibrant vegan eatery known for its innovative and flavorful herb-infused dishes that cater to different dietary preferences.",
+  },
+  {
+    id: 2,
+    name: "Root and Stem",
+    image: rootAndStem,
+    elevation: 2901,
+    featured: false,
+    description:
+      "A trendy and upscale vegan restaurant featuring an eclectic menu focused on root vegetables and plant-based proteins cooked to perfection.",
+  },
+  {
+    id: 3,
+    name: "Garden Delights",
+    image: gardenDelights,
+    elevation: 42,
+    featured: true,
+    description:
+      "A charming and garden-inspired vegan cafe serving delicious and wholesome meals crafted from seasonal produce and artisanal plant-based ingredients.",
+  },
 ];
