93d63a85704fe2142c3a1227aafab56342cf9f5a
Alexis Winters <alexiscodes@yahoo.com>
2023-06-12 15:27:25 -0500

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import './App.css';
-// import { Container, Navbar, NavbarBrand } from "reactstrap";
-// import PlantopiaLogo from "./assets/img/plantopia.png";
-// import RestaurantsList from "./features/restaurants/RestaurantsList";
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import RestaurantsDirectoryPage from './pages/RestaurantsDirectoryPage';
+import RestaurantsDirectoryPage from "./pages/RestaurantsDirectoryPage";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <RestaurantsDirectoryPage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="directory" element={<RestaurantsDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
